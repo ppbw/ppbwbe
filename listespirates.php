902e5790dfbf2da60c18c4c478da9080085250c6
zefredz <zefredz@gmail.com>
2012-09-20 19:59:31 +0200

    add page with candidates lists

---
+++ listespirates.php
@@ -0,0 +1,107 @@
+<?php include("htmlheader.php"); ?>
+    
+    <body>
+    	
+    	<section id="page"> <!-- Defining the #page section with the section tag -->
+    
+		<?php include("pageheader.php"); ?>
+            
+			<section id="articles"> <!-- A new section with the articles -->
+                           		
+				<!-- Article Proposition start -->
+                
+                <article id="Proposition"> <!-- The new article tag. The id is supplied so it can be scrolled into view. -->
+                    <hgroup>
+                    <h2>Election 2012 composition de la liste Pirate</h2>
+                    <h3>Liste 7</h3>
+                    </hgroup>
+                    
+                    <div class="line"></div>
+                    
+                    <div class="articleBody clear">
+                    
+                    <section id="lists">
+                    
+							<div id="ottignies" class="marginLeft25">
+                     
+                    	<hgroup>
+                    	<h3><a name="OLLN" id="OLLN"></a>Commune de Ottignies-Louvain-la-Neuve</h3>
+                    	</hgroup>
+                    	<ol>
+                    	<li> Lionel Dricot (Limelette)</li>
+                    	<li> Valérie Depauw (Louvain-la-Neuve)	</li>
+							<li> Michel Thayse (Louvain-la-Neuve)</li>
+							<li> Fabienne Neuwels (Louvain-la-Neuve), sympathisante</li>
+							<li> Frédéric Minne (Louvain-la-Neuve)	</li>
+							<li> Sophie Lantsoght (Louvain-la-Neuve), sympathisante</li>
+							<li> Alexandre Goffin  (Limelette)</li>
+							<li> Sabine Woitrin (Louvain-la-Neuve)	</li>
+							<li> Antoine Wilmet (Limelette)</li>
+							</ol>
+                    	</div>
+                    	</section>
+                    	<div class="line"></div>                    
+                    
+                     <div id="districtWavre" class="marginLeft25">
+                     
+                    	<hgroup>
+                    	<h3><a name="DISWAV" id="DISWAV"></a>Province de Brabant-Wallon</h3>
+                    	<h4>District de Wavre</h4>
+                    	</hgroup>
+                    	<ol>
+                    	<li> Alexandre Goffin  (Limelette)</li>
+                    	<li> Sabine Woitrin (Louvain-la-Neuve)	</li>
+                    	<li> Michel Thayse (Louvain-la-Neuve)</li>
+                    	<li> Pascale Gorissen (Limal)	</li>
+                    	<li> Antoine Wilmet (Limelette)</li>
+                    	<li> Sophie Lantsoght (Louvain-la-Neuve), sympathisante</li>
+                    	<li> Frédéric Minne (Louvain-la-Neuve)	</li>
+                    	<li> Fabienne Neuwels (Louvain-la-Neuve), sympathisante</li>
+                    	<li> Lionel Dricot (Limelette)</li>
+                    	<li> Valérie Depauw (Louvain-la-Neuve)	</li>
+                    	<li> Adrien le Maire (Lasne)</li>
+                    	</ol>
+                    	</div>
+                    	<div class="line"></div>
+                    	<div id="districtNivelles" class="marginLeft25">
+                     
+                    	<hgroup>
+                    	<h3><a name="DISNIV" id="DISNIV"></a>Province de Brabant-Wallon</h3>
+                    	<h4>District de Nivelles</h4>
+                    	</hgroup>
+                    	<ol>
+                    	<li> Alexandra Coulougnon (Rebecq)</li>
+                    	<li> Alexis Goffin (Braine-l'Alleud)</li>
+                    	<li> Nicolas Ykman (Braine-l'Alleud)</li>
+                    	</ol>
+                    	</div>
+                    	<div class="line"></div>
+                    	
+                    	<p>
+                    		<strong>NB</strong>: une candidate "sympathisante" est une candidate qui ne souhaite pas sièger si elle est élue, 
+                    		mais se présente par sympathie pour que tous les membres d'équipage - de sexe masculin - qui 
+                    		souhaitent se présenter puissent le faire (la loi de parité exige le même nombre d'hommes et de 
+                    		femmes sur une liste, à un candidat prêt).
+                    	</p>
+                    	<p>
+                    		Le Parti Pirate est en effet contre le principe de la liste complète à tout prix, quitte à mettre 
+                    		des personnes peu motivées ou de simples connaissances. Tous les candidats pirates, à l'exception 
+                    		des sympathisants, sont des membres très actifs, participant aux réunions et avec une réelle 
+                    		volonté de s'investir dans la commune
+                    	</p>
+                    	
+                    	</section>
+
+                     </div>
+                     
+                     <a href="#" onclick="history.back(); return false;" class="back"></a>
+                </article>
+                
+				<!-- Article Proposition end -->
+			</section>
+
+			<?php include("footer.php"); ?>
+            
+		</section> <!-- Closing the #page section -->
+    </body>
+</html>
