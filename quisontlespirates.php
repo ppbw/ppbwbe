984657f06e817801062f5c22a0519379f8956ed7
zefredz <zefredz@gmail.com>
2012-09-21 20:16:54 +0200

    add codex pirate

--- quisontlespirates.php
+++ quisontlespirates.php
@@ -30,6 +30,69 @@
 						
 						<p>Plus pratiquement, les Pirates belges sont aussi des équipes, à travers le pays, qui se rassemblent dans une autonomie certaine, de manière à promouvoir la réflexion, les échanges d'idées et les initiatives. En Brabant Wallon, le Crew (l'équipage pirate) est coordonnée par trois personnes mais beaucoup de pirates participent aux réunions mensuelles. Venez nous rencontrer pour discuter !</p>
                     </div>
+                    <div class="line"></div>
+                    <section id="codexPirate">
+					<header>
+					<hgroup>
+					<h2>Nos valeurs :</h2>
+					<h3>Le Code Pirate</h3>
+					</hgroup>
+					</header>
+					
+					<h4>I - Les Pirates sont libres</h4>
+					<p>
+					Les Pirates chérissent la liberté, sont indépendants, autonomes, et refusent toute forme d’obédience 
+					aveugle. Ils affirment le droit à s’informer soi-même et choisir son propre destin, et la liberté 
+					d’opinion. Les Pirates assument la responsabilité qu’induit la liberté.
+					</p>
+					
+					<h4>II - Les Pirates respectent la vie privée</h4>
+					<p>
+					Les Pirates protègent la vie privée. Ils combattent l’obsession croissante de surveillance par l’État 
+					et l’économie, car elle empêche le libre développement de l’individu. Une société libre et démocratique 
+					est impossible sans un espace de liberté, privé et hors-surveillance.
+					</p>   
+					
+					<h4>III - Les Pirates ont l'esprit critique</h4>
+					<p>
+					Les Pirates sont créatifs, curieux, et ne se satisfont pas du statu quo. Ils défient les systèmes, 
+					cherchent des points faibles, et trouvent des façons de les corriger. Les Pirates apprennent de 
+					leurs erreurs.
+					</p>
+					
+					<h4>IV - Les Pirates sont équitables</h4>
+					<p>
+					Les Pirates tiennent leurs promesses. La solidarité est importante quand elle profite à la collectivité. 
+					Les Pirates n’acceptent pas la lâcheté et l’indifférence en société, et agissent quand un courage moral 
+					est nécessaire.
+					</p>
+					
+					<h4>V - Les Pirates respectent l'Homme et l'environnement</h4>
+					<p>
+					Les Pirates sont pacifiques. Ainsi ils rejettent la peine de mort et la destruction de notre environnement. 
+					Les Pirates luttent pour la pérennité de la nature et de ses ressources. Nous n'acceptons aucun brevet sur 
+					le vivant.
+					</p>
+					
+					<h4>VI - Les Pirates sont avides de connaissance</h4>
+					<p>
+					L’accès à l’information, à l’éducation, au savoir comme aux avancées scientifiques doit être illimité. Les 
+					Pirates soutiennent la culture libre et le logiciel libre.
+					</p>
+					
+					<h4>VII - Les Pirates sont solidaires</h4>
+					<p>
+					Les Pirates respectent la dignité humaine. Ils s’engagent pour une société solidaire où les forts défendent 
+					les faibles. Les Pirates défendent une conception de la politique faite d’objectivité et d’équité.					
+					</p>
+					
+					<h4>VIII - Les Pirates sont cosmopolites</h4>
+					<p>
+					Les Pirates font partie d’un mouvement mondial. Ils s’appuient sur les ressources qu’offre Internet, et par 
+					conséquent, pensent et agissent par-delà les frontières.
+					</p>
+                    
+                    </section>
                     
                     <a href="#" onclick="history.back(); return false;" class="back"></a>
                 </article>
