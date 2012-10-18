703979dd7d8833ab8da9573f096de0c67d302a68
zefredz <zefredz@gmail.com>
2012-10-18 19:32:25 +0200

    add next meeting location

--- pageheader.php
+++ pageheader.php
@@ -2,7 +2,7 @@
 <header> <!-- Defining the header section of the page with the appropriate tag -->	
 
 	<div id="news">
-        Rencontre des pirates et debriefing des élections ce <b>vendredi 19 octobre 2012 à 19h30</b> lieu à déterminer.  <b>Feel welcome!</b>
+        Rencontre des pirates et debriefing des élections ce <b>vendredi 19 octobre 2012 à 19h30</b> à la Fattoria, Rue de la Baraque 120 B à Louvain-la-Neuve.  <b>Feel welcome!</b>
     </div>
     </div>
 	<a href="/"><img src="img/Logo-bw.png" alt="logo du ppbw"/></a>
