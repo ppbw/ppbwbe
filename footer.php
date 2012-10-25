5b874669127dfc23811093824ac507a8778258b4
Lionel Dricot <ploum@ploum.net>
2012-10-25 12:57:02 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -13,20 +13,17 @@
 		
 	</p>
 
-
+	<!-- Piwik --> 
+		<script type="text/javascript">
+			var pkBaseURL = (("https:" == document.location.protocol) ? "https://stat.fritalk.com/" : "http://stat.fritalk.com/");
+			document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+			</script><script type="text/javascript">
+			try {
+				var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 24);
+				piwikTracker.trackPageView();
+				piwikTracker.enableLinkTracking();
+			} catch( err ) {}
+		</script><noscript><p><img src="http://stat.fritalk.com/piwik.php?idsite=24" style="border:0" alt="" /></p></noscript>
+	<!-- End Piwik Tracking Code -->
 </footer>
-
-<!-- Piwik -->
-<script type="text/javascript">
-var pkBaseURL = (("https:" ==document.location.protocol) ? "https://stat.fritalk.com/" : "http://stat.fritalk.com/");
-document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js'
-type='text/javascript'%3E%3C/script%3E"));
-</script><script type="text/javascript">
-try {
-var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 24);
-piwikTracker.trackPageView();
-piwikTracker.enableLinkTracking();
-} catch( err ) {}
-</script><noscript><p><img src="http://stat.fritalk.com/piwik.php?idsite=24" style="border:0" alt="" /></p></noscript>
-<!-- End Piwik Tracking Code -->
 
